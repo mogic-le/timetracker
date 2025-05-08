91f5174923b2380a157786ea7f59a727634e47c0
Timothy Elias <elias@mogic.com>
2025-05-08 11:58:14 +0200

    TIM-123: Add new route get single entry by id: /tracking/entry/{id}

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -23,6 +23,58 @@ class CrudController extends BaseController
 {
     private \Psr\Log\LoggerInterface $logger;
 
+    public function getAction(Request $request)
+    {
+        if (!$this->checkLogin($request)) {
+            return $this->getFailedLoginResponse();
+        }
+
+        // type and syntax check in routing.yml
+        $entryId = $request->get('id');
+
+        $doctrine = $this->getDoctrine();
+        /** @var Entry $entry */
+        $entry = $doctrine->getRepository(\App\Entity\Entry::class)
+            ->find($entryId);
+
+        if (!$entry) {
+            $message = $this->translator->trans('No entry for id.');
+            return new Error($message, 404);
+        }
+
+        // check for role, dev can only return their own tickets
+        $ticketUserId = $entry->getUser() ? $entry->getUser()->getId() : null;
+        $currentUserId = $this->getUserId($request);
+        if ($this->isDEV($request) && ($ticketUserId != $currentUserId)) {
+            $message = $this->translator->trans('You are not allowed to view this entry.');
+            return new Error($message, 403);
+        }
+
+        // format output like /interpretation/allEntries
+        $date = $entry->getDay() ? $entry->getDay()->format('Y-m-d') : null;
+        $entry = $entry->toArray();
+
+        // unset unneded keys
+        unset($entry['class']);
+        $entry['date'] = $date;
+
+        // add id suffix to id parameter
+        $entry['user_id'] = $entry['user'];
+        $entry['project_id'] = $entry['project'];
+        $entry['customer_id'] = $entry['customer'];
+        $entry['activity_id'] = $entry['activity'];
+        $entry['worklog_id'] = $entry['worklog'];
+
+        // unset old keys
+        unset($entry['user']);
+        unset($entry['project']);
+        unset($entry['customer']);
+        unset($entry['activity']);
+        unset($entry['worklog']);
+
+        return new JsonResponse(array('data' => $entry));
+    }
+
     /**
      * @required
      * @codeCoverageIgnore
