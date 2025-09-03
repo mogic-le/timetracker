d61418fb1090ac0ee0b962a9646c2cab4a07a90e
Timothy Elias <elias@mogic.com>
2025-09-03 09:41:33 +0200

    TIM-124: Disable profiler for route rendered in iframe

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Model\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
 
 class StatusController extends BaseController
 {
@@ -26,8 +27,12 @@ class StatusController extends BaseController
         return new JsonResponse($this->getStatus());
     }
 
-    public function pageAction(): \Symfony\Component\HttpFoundation\Response
+    public function pageAction(?Profiler $profiler): \Symfony\Component\HttpFoundation\Response
     {
+
+        // disable the profiler in the iframe
+        $profiler?->disable();
+
         $status = $this->getStatus();
 
         return $this->render('status.html.twig', [
