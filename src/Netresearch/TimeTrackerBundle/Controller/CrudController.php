8a7298125b4414cd6093517be7ca01a304e1a4f2
Timothy Elias <elias@mogic.com>
2025-10-29 10:38:04 +0100

    TIM-132: Parameter activity is required when creating a new entry
     - Entries without activity are malformed and throw errors

--- src/Netresearch/TimeTrackerBundle/Controller/CrudController.php
+++ src/Netresearch/TimeTrackerBundle/Controller/CrudController.php
@@ -242,6 +242,17 @@ class CrudController extends BaseController
             return $this->getFailedLoginResponse();
         }
 
+        // Check activityId is not null
+        if (!$activityId = (int) $request->get('activity')) {
+            $message = $this->get('translator')->trans(
+                "The activityId '%activity%', is not valid, must be a positive integer.",
+                [
+                    '%activity%' => $request->get('activity'),
+                ]
+            );
+            return new Error($message, 400);
+        }
+
         try {
             $alert = null;
             $this->logDataToFile($_POST, TRUE);
@@ -312,9 +323,15 @@ class CrudController extends BaseController
                 }
             }
 
+            // Check activityId belongs to activity
             /** @var Activity $activity */
-            if ($activity = $doctrine->getRepository('NetresearchTimeTrackerBundle:Activity')->find($request->get('activity'))) {
+            if ($activity = $doctrine->getRepository('NetresearchTimeTrackerBundle:Activity')->find($activityId)) {
                 $entry->setActivity($activity);
+            } else {
+                $message = $this->get('translator')->trans(
+                    "The activityId " . $activityId . ", does not match any known activity."
+                );
+                return new Error($message, 422);
             }
 
             $entry->setTicket(strtoupper(trim($request->get('ticket') ? $request->get('ticket') : '')))
